090b43dbdef8e36d1283e66c62c7b6157e771498
nguyen hoang <traudat2212@gmail.com>
2023-06-03 16:05:20 +0700

    Initial commit

---
+++ src/main/java/com/lab/contactsmanagement/ContactsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.lab.contactsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/contactsmanagement/ContactsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.contactsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
